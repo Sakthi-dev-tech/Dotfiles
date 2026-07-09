ec68db01688a13ff0e9d1242b1e6bfe2954c6857
Sakthi-PC <mssakthieesvar@gmail.com>
2026-07-09 18:16:05 +0800

    Added wallpaper for ghostty

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -58,6 +58,7 @@ local menu        = "hyprlauncher"
 hl.on("hyprland.start", function ()
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
+  hl.exec_cmd("hyprpaper")
 end)
 
 -------------------------------
