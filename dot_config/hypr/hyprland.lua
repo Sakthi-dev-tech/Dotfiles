a67759b952fa8a9ed6656f2cb62c984ff29a3b25
Sakthi-PC <mssakthieesvar@gmail.com>
2026-07-09 18:22:16 +0800

    waypaper --restore added

--- dot_config/hypr/hyprland.lua
+++ dot_config/hypr/hyprland.lua
@@ -58,7 +58,7 @@ local menu        = "hyprlauncher"
 hl.on("hyprland.start", function ()
 	hl.exec_cmd("wl-paste --type text --watch cliphist store")
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
-  hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("waypaper --restore")
 end)
 
 -------------------------------
