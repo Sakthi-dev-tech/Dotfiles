9c915217069167e98e0ae4a79adfe46c3599a672
Sakthi-PC <mssakthieesvar@gmail.com>
2026-07-22 12:11:36 +0800

    added nvim logo in lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -2,6 +2,16 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     require("lualine").setup({
+      sections = {
+        lualine_a = {
+          {
+            "mode",
+            fmt = function (str)
+              return ' ' .. str
+            end
+          }
+        }
+      },
       option = {
         theme = 'dracula',
         globalstatus = true
