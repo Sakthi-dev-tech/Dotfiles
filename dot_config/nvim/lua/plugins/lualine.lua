1d62b2c16e43c379ddafa3913ac6bbdfea525f31
Sakthi-PC <mssakthieesvar@gmail.com>
2026-07-23 18:42:45 +0800

    changed vim logo in lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -7,7 +7,7 @@ return {
           {
             "mode",
             fmt = function (str)
-              return ' ' .. str
+              return ' ' .. str
             end
           }
         }
