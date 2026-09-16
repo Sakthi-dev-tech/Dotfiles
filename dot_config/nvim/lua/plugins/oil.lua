c32515cdc2749755521eae35de503322cd0b32ac
Sakthi <sakthi@Sakthi-Lenovo>
2026-09-16 10:23:50 +0800

    Add other plugins for oil nvim

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -1,27 +1,44 @@
 return {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
-    dependencies = {
-        { "nvim-mini/mini.icons", opts = {} },
-        "refractalize/oil-git-status.nvim",
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        opts = {},
+        -- Optional dependencies
+        dependencies = {
+            { "nvim-mini/mini.icons", opts = {} },
+        },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        config = function()
+            require("oil").setup({
+                keymaps = {
+                    ["h"] = { "actions.parent", mode = "n" },
+                    ["."] = { "actions.toggle_hidden", mode = "n" },
+                    ["l"] = { "actions.select" }
+                }
+            })
+            vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+        end
     },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-    config = function()
-        require("oil").setup({
-            win_options = {
-                signcolumn = "yes:2",
-            },
-            keymaps = {
-                ["-"] = { "actions.parent", mode = "n" },
-                ["."] = { "actions.toggle_hidden", mode = "n" },
-            }
-        })
-        require("oil-git-status").setup()
-        vim.keymap.set("n", "<leader>e", "<CMD>Oil<CR>", {desc = "Open parent directory"})
-    end
+    {
+        "benomahony/oil-git.nvim",
+        dependencies = { "stevearc/oil.nvim" },
+        -- No opts or config needed! Works automatically
+    },
+    {
+        "JezerM/oil-lsp-diagnostics.nvim",
+        dependencies = { "stevearc/oil.nvim" },
+        opts = {}
+    },
+    {
+        "refractalize/oil-git-status.nvim",
+
+        dependencies = {
+            "stevearc/oil.nvim",
+        },
+
+        config = true,
+    },
 }
